07bbf4b14db1019e035fd8be6306ddd5de5ca6d6
kyosti <kyosti.puro@vincit.fi>
2021-06-10 22:27:36 +0300

    Adds support for changing between list or grid of images

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,10 +1,11 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator, StackNavigationOptions } from '@react-navigation/stack';
 import * as React from 'react';
 
 import { ListScreen } from '../screens/ListScreen';
 import { ImageScreen } from '../screens/ImageScreen';
 import { CameraScreen } from '../screens/CameraScreen';
+import { View } from 'react-native';
 
 export type RootStackParamList = {
   List: undefined;
@@ -14,12 +15,18 @@ export type RootStackParamList = {
 
 const RootStack = createStackNavigator<RootStackParamList>();
 
+const transparentHeaderOptions: StackNavigationOptions = {
+  headerTransparent: true,
+  headerBackground: (p) => <View style={[p.style, { backgroundColor: "#0009", width: "100%", height: "100%" }]} />,
+  headerTintColor: "#fff",
+};
+
 function RootNavigator() {
   return (
     <RootStack.Navigator initialRouteName="List">
-      <RootStack.Screen name="List" component={ListScreen} />
-      <RootStack.Screen name="Image" component={ImageScreen} />
-      <RootStack.Screen name="Camera" component={CameraScreen} />
+      <RootStack.Screen name="List" component={ListScreen}/>
+      <RootStack.Screen name="Image" component={ImageScreen} options={transparentHeaderOptions} />
+      <RootStack.Screen name="Camera" component={CameraScreen} options={transparentHeaderOptions} />
     </RootStack.Navigator>
   );
 }
--- src/screens/ListScreen.tsx
+++ src/screens/ListScreen.tsx
@@ -1,6 +1,7 @@
+import { Fontisto } from '@expo/vector-icons';
 import { StackNavigationProp } from '@react-navigation/stack';
-import React, { useCallback, useMemo } from 'react';
-import { StyleSheet, View, Image, Button, Dimensions } from 'react-native';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import { StyleSheet, View, Image, Button, Dimensions, Text } from 'react-native';
 import { FlatList, TouchableOpacity } from 'react-native-gesture-handler';
 import { RootStackParamList } from '../navigation';
 import { useStoreContext, IImage } from '../services';
@@ -9,7 +10,7 @@ const dateSort = (a: Date, b: Date) => {
   return b.getTime() - a.getTime();
 }
 
-function ImagePreviewComponent({ img, id, open }: { img: IImage, id: string, open: (id: string) => void }) {
+function ImageGridPreviewComponent({ img, id, open }: { img: IImage, id: string, open: (id: string) => void }) {
   const width = (Dimensions.get('window').width / 3);
   return (
     <TouchableOpacity onPress={() => open(id)} style={{ width: width, height: width }} >
@@ -18,11 +19,25 @@ function ImagePreviewComponent({ img, id, open }: { img: IImage, id: string, ope
   )
 }
 
+function ImageListPreviewComponent({ img, id, open }: { img: IImage, id: string, open: (id: string) => void }) {
+  const width = Dimensions.get('window').width;
+  return (
+    <TouchableOpacity onPress={() => open(id)} style={{ width: width, height: width }} >
+      <Image source={{ uri: img.uri }} style={{ width: width - 2, height: width - 2, margin: 1 }} />
+      <View style={{ position: "absolute", bottom: 0, backgroundColor: "#0005", width: "100%" }}>
+        { img.caption && <Text style={{ color: "#fff", fontSize: 20, margin: 5 }}>{img.caption}</Text> }
+      </View>
+    </TouchableOpacity>
+  )
+}
+
+
 type ListScreenNavigationProp = StackNavigationProp<
   RootStackParamList,
   'List'
 >;
 
+type ListType = "grid" | "list";
 export function ListScreen({ navigation }: { navigation: ListScreenNavigationProp }) {
   const store = useStoreContext();
   const open = useCallback((id: string) => navigation.navigate("Image", { id }), [navigation]);
@@ -30,15 +45,34 @@ export function ListScreen({ navigation }: { navigation: ListScreenNavigationPro
     return Object.entries(store.images) // Vaihda listan tyyppiä?
       .sort(([idA, a], [idB, b]) => dateSort(a.createdAt, b.createdAt))
   }, [store.images]);
+  const [listType, setListType] = useState<ListType>("grid")
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: (props) => {
+        return (
+          <TouchableOpacity onPress={() => setListType((state) => state === "grid" ? "list" : "grid" )} >
+            {
+              listType === "grid" ?
+                <Fontisto name="nav-icon" style={{ marginRight: 20, fontSize: 18 }}/> :
+                <Fontisto name="nav-icon-grid" style={{ marginRight: 20, fontSize: 18 }} />
+            }
+          </TouchableOpacity>
+        );
+      }
+    });
+  }, [listType])
   return (
     <View style={styles.imageContainer}>
       <FlatList
-        numColumns={3}
+        key={listType}
+        numColumns={listType === "grid" ? 3 : 1}
         extraData={open}
         data={images}
         renderItem={(item) => {
           const [id, img] = item.item;
-          return <ImagePreviewComponent key={id} img={img} id={id} open={open} />;
+          return listType === "grid" ?
+            <ImageGridPreviewComponent key={id} img={img} id={id} open={open} /> :
+            <ImageListPreviewComponent key={id} img={img} id={id} open={open} />;
         }}
         keyExtractor={([id]) => id}
       />
