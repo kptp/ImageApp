2f5d8d5a4702fdc3df5649a24da1df6aa4e750a4
kyosti <kyosti.puro@vincit.fi>
2021-06-12 19:44:50 +0300

    Moves camera permission checking to its own component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
-import React, { useState, useEffect } from "react";
-import { Text, View } from "react-native";
-import { Camera } from "expo-camera";
+import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { StoreProvider } from "./stores";
 import Navigation from "./navigation";
 
 export default function App(): React.ReactElement {
-  const [hasPermission, setHasPermission] = useState<boolean | null>(null);
-
-  useEffect(() => {
-    (async () => {
-      const { status } = await Camera.requestPermissionsAsync();
-      setHasPermission(status === "granted");
-    })();
-  }, []);
-
-  if (hasPermission === null) {
-    return <View />;
-  }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
-  }
   return (
     <SafeAreaProvider>
       <StoreProvider>
--- src/screens/CameraScreen.tsx
+++ src/screens/CameraScreen.tsx
@@ -1,13 +1,37 @@
 import { useIsFocused } from "@react-navigation/core";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { Camera } from "expo-camera";
-import React, { useCallback, useRef } from "react";
-import { StyleSheet, View, Button, Dimensions } from "react-native";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import { StyleSheet, View, Button, Dimensions, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { RootStackParamList } from "../navigation/types";
 import { useStoreContext, randomId } from "../stores";
 
 type CameraScreenNavigationProp = StackNavigationProp<RootStackParamList, "Camera">;
+
+export function WithCameraPermission({ children }: { children: React.ReactElement }): React.ReactElement {
+  const [hasPermission, setHasPermission] = useState<boolean | null>(null);
+  const fetchPermission = useCallback(async () => {
+    const { status } = await Camera.requestPermissionsAsync();
+    setHasPermission(status === "granted");
+  }, []);
+  useEffect(() => {
+    fetchPermission();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return (
+      <View style={styles.noPermission}>
+        <Text style={styles.noPermissionText}>No access to camera</Text>
+        <Button onPress={fetchPermission} title="Request permission" />
+      </View>
+    );
+  }
+  return children;
+}
 
 export function CameraScreen({ navigation }: { navigation: CameraScreenNavigationProp }): React.ReactElement {
   const store = useStoreContext();
@@ -34,7 +58,11 @@ export function CameraScreen({ navigation }: { navigation: CameraScreenNavigatio
         {
           // We need to unmount the camera if focus is lost, otherwise it will use unnecessary resources
           // and cause bugs when returning from image preview
-          isFocused && <Camera style={styles.camera} type={Camera.Constants.Type.back} ref={cameraRef} ratio="16:9" />
+          isFocused && (
+            <WithCameraPermission>
+              <Camera style={styles.camera} type={Camera.Constants.Type.back} ref={cameraRef} ratio="16:9" />
+            </WithCameraPermission>
+          )
         }
         <Button title="Take pic" onPress={takePicture} />
       </View>
@@ -53,5 +81,15 @@ const styles = StyleSheet.create({
     flexBasis: (Dimensions.get("window").width / 9) * 16,
     width: Dimensions.get("window").width,
     flexGrow: 0,
+  },
+  noPermission: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  noPermissionText: {
+    fontSize: 20,
+    color: "#fff",
+    margin: 5,
   },
 });
